d44f5b6496537948828f626ddd6c1445c2b508e8
Nikolay <nikolayhudyakov18@gmail.com>
2023-06-26 16:24:22 +0400

    Edit ZXingCameraResolutionConverter

--- UploadApp/UploadApp/Converters/ZXingCameraResolutionConverter.cs
+++ UploadApp/UploadApp/Converters/ZXingCameraResolutionConverter.cs
@@ -13,17 +13,16 @@ namespace UploadApp.Converters
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var cameraResolution = (CameraResolution)value;
-            return cameraResolution != null ? $"{cameraResolution.Width} * {cameraResolution.Height}" : string.Empty;
+            return cameraResolution != null ? $"{cameraResolution.Width} * {cameraResolution.Height}" : "0 * 0";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             string[] cameraResolution = value.ToString().Split("*");
-
-            if (cameraResolution.Length == 2 && )
-                return new CameraResolution();
-            
-            return new CameraResolution(){ Width = int.Parse(cameraResolution[0]), Height = int.Parse(cameraResolution[1]) };
+            int width, height;
+            if (cameraResolution.Length == 2 && int.TryParse(cameraResolution[0], out width) & int.TryParse(cameraResolution[1], out height))
+                return new CameraResolution() { Width = width, Height = height };
+            return new CameraResolution();
         }
     }
 }
